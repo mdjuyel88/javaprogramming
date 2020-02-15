d267367d64b934c214674cb820ca3bfd515c3e0d
user <user@user-PC>
2020-02-15 11:52:10 +0530

    add 2 numbers using scanner input

---
+++ juyel/src/juyel/jav2.java
@@ -0,0 +1,18 @@
+package juyel;
+import java.util.*;
+public class jav2 {
+	public static void main(String args[])
+	{
+		int a,b,c;
+		Scanner sn=new Scanner(System.in);
+		System.out.println("Enter 1st number=");
+		a = sn.nextInt();
+		System.out.println("Enter 1st number=");
+		b=sn.nextInt();
+		c=a+b;
+		System.out.println("Sum="+c);
+		
+		
+	}
+
+}
